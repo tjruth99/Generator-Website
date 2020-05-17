3e9dc08e3a58925c8badf6d31aa692784229fbc3
Tyler Ruth <tyler1hero@sbcglobal.net>
2020-05-16 23:32:10 -0400

    added basic random color generation

--- generator/src/generator/colors/colors.jsx
+++ generator/src/generator/colors/colors.jsx
@@ -4,10 +4,39 @@ import { Button, Collapse, Form } from "react-bootstrap";
 
 import "../generator.css";
 
+class ColorDisplay extends React.Component {
+  constructor() {
+    super();
+    this.state = { hex: "#000000" };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.hex !== this.props.hex) {
+      this.setState({
+        hex: this.props.hex,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <div id="colorResult" style={{ background: this.state.hex }}></div>
+      </>
+    );
+  }
+}
+
 class Colors extends React.Component {
   constructor() {
     super();
-    this.state = { settingsShow: false };
+    this.state = {
+      red: 0,
+      green: 0,
+      blue: 0,
+      hex: "#000000",
+      settingsShow: false,
+    };
   }
 
   setSettingsShow = (visibility) => {
@@ -16,7 +45,37 @@ class Colors extends React.Component {
     });
   };
 
-  generateColor() {}
+  generateColor() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+
+    var redHex = red.toString(16);
+    console.log(redHex);
+    if (redHex.length < 2) {
+      redHex = "0" + redHex;
+    }
+
+    var greenHex = green.toString(16);
+    if (greenHex.length < 2) {
+      greenHex = "0" + greenHex;
+    }
+
+    var blueHex = blue.toString(16);
+    if (blueHex.length < 2) {
+      blueHex = "0" + blueHex;
+    }
+
+    var newHex = "#";
+    newHex = newHex.concat(redHex, greenHex, blueHex).toUpperCase();
+
+    this.setState({
+      red: red,
+      green: green,
+      blue: blue,
+      hex: newHex,
+    });
+  }
 
   render() {
     return (
@@ -26,8 +85,18 @@ class Colors extends React.Component {
           Generate a random color or a color palette. Choose from different
           palettes to customize the feel.
         </p>
+
         <div className="resultContainer">
-          <div className="result"></div>
+          <div className="result">
+            <ColorDisplay hex={this.state.hex} />
+            <p id="colorNumResult">
+              <b>
+                R: {this.state.red} G: {this.state.green} B: {this.state.blue}
+              </b>
+              <br />
+              <b>Hex: {this.state.hex}</b>
+            </p>
+          </div>
         </div>
 
         <Button onClick={() => this.generateColor()}>Generate</Button>
