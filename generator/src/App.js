a78f9f46dae101d7f7f1b6d8f3475cd2c49e1372
Tyler Ruth <tyler1hero@sbcglobal.net>
2020-05-22 02:49:19 -0400

    deployed to github pages

--- generator/src/App.js
+++ generator/src/App.js
@@ -1,14 +1,86 @@
 import React from "react";
-import Main from "./generator/main.jsx";
+
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Numbers from "./generator/numbers/numbers.jsx";
+import Colors from "./generator/colors/colors.jsx";
+import SequenceElement from "./generator/sequences/sequences-element.jsx";
+import SequenceRandomize from "./generator/sequences/sequences-randomize.jsx";
+
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="App">
+          <Router basename="/">
+            <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+              <Navbar.Brand>
+                <Link to="/" style={{ textDecoration: "none" }}>
+                  Generator
+                </Link>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link>
+                    <Link to="/numbers" style={{ textDecoration: "none" }}>
+                      Numbers
+                    </Link>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Link to="/colors" style={{ textDecoration: "none" }}>
+                      Colors
+                    </Link>
+                  </Nav.Link>
+                  <NavDropdown title="Sequences" id="sequences-dropdown">
+                    <NavDropdown.Item>
+                      <Link
+                        to="/sequences/element"
+                        style={{ textDecoration: "none" }}
+                      >
+                        Random Element
+                      </Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item>
+                      <Link
+                        to="/sequences/randomize"
+                        style={{ textDecoration: "none" }}
+                      >
+                        Randomize Sequence
+                      </Link>
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+            <br />
+            <Switch>
+              <Route exact path="/">
+                Home page WIP
+              </Route>
+              <Route path="/numbers">
+                <Numbers />
+              </Route>
+              <Route path="/colors">
+                <Colors />
+              </Route>
+              <Route path="/sequences/element">
+                <SequenceElement />
+              </Route>
+              <Route path="/sequences/randomize">
+                <SequenceRandomize />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
