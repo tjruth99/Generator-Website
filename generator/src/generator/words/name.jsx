945210a67ac184b6ff91d1f2f36db8d1ae8df272
Tyler Ruth <tyler1hero@sbcglobal.net>
2020-05-30 22:43:29 -0400

    added name generator

--- generator/src/generator/words/name.jsx
+++ generator/src/generator/words/name.jsx
@@ -2,12 +2,27 @@ import React from "react";
 
 import { Button, Collapse, Form } from "react-bootstrap";
 
+import enFirstNames from "./us-first.js";
+import enLastNames from "./us-last.js";
+
 import "../generator.css";
+
+let firstNames = [];
+let lastNames = [];
 
 class Name extends React.Component {
   constructor() {
     super();
-    this.state = { settingsShow: false };
+    this.state = {
+      result: "Generate Name",
+      animate: false,
+      settingsShow: false,
+    };
+  }
+
+  componentDidMount() {
+    firstNames = enFirstNames.split(/\r?\n/);
+    lastNames = enFirstNames.split(/\r?\n/);
   }
 
   setSettingsShow = (visibility) => {
@@ -16,7 +31,15 @@ class Name extends React.Component {
     });
   };
 
-  generateName() {}
+  generateName() {
+    let first = firstNames[Math.floor(Math.random() * firstNames.length)];
+    let last = lastNames[Math.floor(Math.random() * lastNames.length)];
+
+    this.setState({
+      animate: true,
+      result: first + " " + last,
+    });
+  }
 
   render() {
     return (
@@ -24,7 +47,15 @@ class Name extends React.Component {
         <h1>Name</h1>
         <p className="description-text">Generate a random name.</p>
         <div className="result-container">
-          <div className="result"></div>
+          <div
+            className={
+              this.state.animate ? "result result-string-animation" : "result"
+            }
+            id="result-string"
+            onAnimationEnd={() => this.setState({ animate: false })}
+          >
+            {this.state.result}
+          </div>
         </div>
 
         <Button onClick={() => this.generateName()}>Generate</Button>
