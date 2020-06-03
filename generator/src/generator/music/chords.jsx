b66070bc6600e2114a0a31f6e264755dde7eb39f
Tyler Ruth <tyler1hero@sbcglobal.net>
2020-06-02 22:21:23 -0400

    added settings to chord generator

--- generator/src/generator/music/chords.jsx
+++ generator/src/generator/music/chords.jsx
@@ -1,14 +1,69 @@
 import React from "react";
 
-import { Button, Collapse, Form } from "react-bootstrap";
+import { Button, Col, Collapse, Form, Row } from "react-bootstrap";
 
 import "../generator.css";
+
+const notes = [
+  "A",
+  "A#/Bb",
+  "B",
+  "C",
+  "C#/Db",
+  "D",
+  "D#/Eb",
+  "E",
+  "F",
+  "F#/Gb",
+  "G",
+  "G#/Ab",
+];
+
+const MAX_SIZE = 12;
 
 class ChordGenerator extends React.Component {
   constructor() {
     super();
-    this.state = { result: [], settingsShow: false };
+    this.state = {
+      result: [],
+      key: 0,
+      major: true,
+      size: 4,
+      settingsShow: false,
+    };
   }
+
+  handleSizeChange = (event) => {
+    var newSize = parseInt(event.target.value);
+    if (newSize > MAX_SIZE) {
+      newSize = MAX_SIZE;
+    } else if (newSize < 1) {
+      newSize = 1;
+    }
+
+    this.setState({
+      size: newSize,
+    });
+  };
+
+  handleTonalityChange = (event) => {
+    let isMajor = event.target.value === "Major";
+    this.setState({ major: isMajor });
+  };
+
+  handleKeyChange = (event) => {
+    let selected = event.target.value;
+    var type = -1;
+    for (var i = 0; i < notes.length; i++) {
+      if (selected === notes[i]) {
+        type = i;
+      }
+    }
+
+    this.setState({
+      key: type,
+    });
+  };
 
   setSettingsShow = (visibility) => {
     this.setState({
@@ -27,6 +82,10 @@ class ChordGenerator extends React.Component {
           <div className="result">{this.state.result}</div>
         </div>
 
+        <div>
+          State: <br /> {this.state.major ? "True" : "False"}
+        </div>
+
         <Button onClick={() => this.generateChords()}>Generate</Button>
         <br />
 
@@ -41,7 +100,46 @@ class ChordGenerator extends React.Component {
         <br />
         <div className="settings">
           <Collapse in={this.state.settingsShow} fluid>
-            <Form></Form>
+            <Form>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>
+                  Key:
+                </Form.Label>
+                <Col>
+                  <Form.Control as="select" onChange={this.handleKeyChange}>
+                    {notes.map((i) => (
+                      <option key={i}>{i}</option>
+                    ))}
+                  </Form.Control>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>
+                  Tonality:
+                </Form.Label>
+                <Col>
+                  <Form.Control
+                    as="select"
+                    onChange={this.handleTonalityChange}
+                  >
+                    <option>Major</option>
+                    <option>Minor</option>
+                  </Form.Control>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>
+                  Number of chords to generate:
+                </Form.Label>
+                <Col>
+                  <Form.Control
+                    type="number"
+                    value={this.state.size}
+                    onChange={this.handleSizeChange}
+                  />
+                </Col>
+              </Form.Group>
+            </Form>
           </Collapse>
         </div>
       </>
